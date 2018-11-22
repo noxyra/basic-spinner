e14bd1330fb35bf1e63a58238154358b2e9d263f
noxyra <noxyra@gmail.com>
2018-11-22 11:48:23 +0100

    New functionnality :
    - Vars replacer
    - MicroSpin injector

--- src/Spinner/Spinner.php
+++ src/Spinner/Spinner.php
@@ -4,33 +4,98 @@ namespace Noxyra\BasicSpinner\Spinner;
 
 abstract class Spinner
 {
-    protected const API_ENDPOINT = "https://api.datamuse.com/words?";
+    private $arrayMicroMasterSpin;
+
+    public function __construct($arrayMicroMasterSpin = [])
+    {
+        /* The array format : [
+            'animal|pet',                   // Required
+            'Hi !|Key !|Good morning.|',    // or empty possibility
+        ] */
+        $this->arrayMicroMasterSpin = $arrayMicroMasterSpin;
+    }
 
     /**
-     * Execute the spin
+     * Main function for build the text
      *
      * @param string $text
+     * @param array $replaceVarsArray
+     * @param bool $autoInject
      * @return string
      */
-    public static function execute(string $text)
+    public function generateText(string $text, array $replaceVarsArray = [], bool $autoInject = true)
+    {
+        if ($autoInject) {
+            $text = $this->autoInjectMicroMasterSpins($text);
+        }
+
+        if (!empty($replaceVarsArray)) {
+            $text = self::replaceVars($text, $replaceVarsArray);
+        }
+
+        return self::execute($text);
+    }
+
+    /**
+     * Execute the masterspin
+     *
+     * @param string $masterSpin
+     * @return string
+     */
+    public static function execute(string $masterSpin)
     {
         $pattern = '#\{([^{}]*)\}#msi';
-        $test = preg_match_all($pattern, $text, $out);
-        if(!$test)
-            return $text;
+        $test = preg_match_all($pattern, $masterSpin, $out);
+
+        if (!$test) {
+            return $masterSpin;
+        }
 
         $toFind = array();
         $toReplace = array();
 
-        foreach($out[0] as $id => $match)
-        {
+        foreach ($out[0] as $id => $match) {
             $select = explode("|", $out[1][$id]);
             $toFind[] = $match;
-            $toReplace[] = $select[rand(0, count($select)-1)];
+            $toReplace[] = $select[rand(0, count($select) - 1)];
         }
-        $response = str_replace($toFind, $toReplace, $text);
+
+        $response = str_replace($toFind, $toReplace, $masterSpin);
 
         return self::execute($response);
 
+    }
+
+    /**
+     * Replace words by vars specified in $vars
+     *
+     * @param string $text
+     * @param array $vars
+     * @return string
+     */
+    public static function replaceVars(string $text, $vars = [])
+    {
+        foreach ($vars as $varName => $varValue) {
+            str_replace('$' . $varName . '$', $varValue, $text);
+        }
+
+        return $text;
+    }
+
+    /**
+     * Use the array gived in constructor for replace specifics words by masterspin expressions
+     *
+     * @param string $text
+     * @return mixed|string
+     */
+    public function autoInjectMicroMasterSpins(string $text)
+    {
+        foreach ($this->arrayMicroMasterSpin as $matchString) {
+            $matchArray = explode('|', $matchString);
+            foreach ($matchArray as $matchWord) {
+                $text = str_replace($matchWord, '{' . $matchString . '}', $text);
+            }
+        }
+        return $text;
     }
 }
